49de7f887093772634289b5db174bc52525c9bf9
Artur Guli <arturgulinski@gmail.com>
2025-07-27 19:36:35 +0200

    Fix: stable build with wallet adapter

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import App from "./App";
 
-// Wallet setup
-import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import {
+  ConnectionProvider,
+  WalletProvider
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider
+} from "@solana/wallet-adapter-react-ui";
+import {
+  PhantomWalletAdapter
+} from "@solana/wallet-adapter-wallets";
 import { clusterApiUrl } from "@solana/web3.js";
 
+import "@solana/wallet-adapter-react-ui/styles.css"; // required for modal styles
+
 const endpoint = clusterApiUrl("devnet");
+
 const wallets = [new PhantomWalletAdapter()];
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ConnectionProvider endpoint={endpoint}>
-    <WalletProvider wallets={wallets} autoConnect>
-      <WalletModalProvider>
-        <App />
-      </WalletModalProvider>
-    </WalletProvider>
-  </ConnectionProvider>
+  <React.StrictMode>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <App />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  </React.StrictMode>
 );

6e51a491c259112d35d775445a75053361bb316d
Artur Guli <arturgulinski@gmail.com>
2025-07-28 13:07:21 +0200

    Fix JSX parsing: renamed App.js → App.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,6 @@
-// main.jsx (final)
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./src/App.js";
+import App from "./src/App.jsx"; 
 import "./src/App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
